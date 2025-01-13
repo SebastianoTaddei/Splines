2aef1501c2eaff14f52e145248740ade395c3039
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-01-13 09:16:54 +0100

    Added install instructions and improved setup.rb

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# Clothoids
+# Splines
 
 Fork of [Splines](https://github.com/ebertolazzi/Splines) by Enrico Bertolazzi.
 
 This is just a fork that tries its best to keep stable releases and stay up-to-date.
 
 Unless it relates directly to something here (e.g., a broken release) please refer to the original repo!
+
+## Installation
+
+In each release you can find precompiled binaries for Windows, Linux and MacOS. You can also compile the library yourself, but first you need to install the dependencies:
+
+- ruby >=2.6 with the following gems: rake, colorize, rubyzip;
+- CMake with ninja;
+- python 3.8-13 (other versions are untested).
+
+Once those requirements are installed, simply:
+
+```bash
+git clone --branch stable --depth 1 https://github.com/SebastianoTaddei/Splines.git
+cd Splines
+ruby setup.rb
+rake
+```
--- setup.rb
+++ setup.rb
@@ -4,11 +4,29 @@ puts "Setup submodules"
 Dir.chdir(File.dirname(__FILE__))
 
 # Clone submodules
-system('git clone --branch 1.0.0 --depth 1 https://github.com/SebastianoTaddei/cmake_utils.git cmake_utils')
-system('git clone --branch 1.1.2 --depth 1 https://github.com/SebastianoTaddei/GenericContainer.git submodules/GenericContainer')
-system('git clone --branch 1.0.2 --depth 1 https://github.com/SebastianoTaddei/UtilsLite.git submodules/UtilsLite')
-system('git clone --branch 1.1.2 --depth 1 https://github.com/SebastianoTaddei/quarticRootsFlocke.git submodules/quarticRootsFlocke')
+if not File.directory?('cmake_utils')
+  system('git clone --branch 1.0.0 --depth 1 https://github.com/SebastianoTaddei/cmake_utils.git cmake_utils')
+else
+  puts "cmake_utils already cloned. If you want to update it, delete the folder and run this script again."
+end
 
-system('ruby submodules/GenericContainer/setup.rb')
-system('ruby submodules/UtilsLite/setup.rb')
-system('ruby submodules/quarticRootsFlocke/setup.rb')
+if not File.directory?('submodules/GenericContainer')
+  system('git clone --branch 1.1.2 --depth 1 https://github.com/SebastianoTaddei/GenericContainer.git submodules/GenericContainer')
+  system('ruby submodules/GenericContainer/setup.rb')
+else
+  puts "GenericContainer already cloned. If you want to update it, delete the folder and run this script again."
+end
+
+if not File.directory?('submodules/UtilsLite')
+  system('git clone --branch 1.0.2 --depth 1 https://github.com/SebastianoTaddei/UtilsLite.git submodules/UtilsLite')
+  system('ruby submodules/UtilsLite/setup.rb')
+else
+  puts "UtilsLite already cloned. If you want to update it, delete the folder and run this script again."
+end
+
+if not File.directory?('submodules/quarticRootsFlocke')
+  system('git clone --branch 1.1.2 --depth 1 https://github.com/SebastianoTaddei/quarticRootsFlocke.git submodules/quarticRootsFlocke')
+  system('ruby submodules/quarticRootsFlocke/setup.rb')
+else
+  puts "quarticRootsFlocke already cloned. If you want to update it, delete the folder and run this script again."
+end
