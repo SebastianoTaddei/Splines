f10cda93fac91d7749ba05f63e1a547736060a47
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-01-13 09:42:04 +0100

    Fixed name of action and bumped versions

--- setup.rb
+++ setup.rb
@@ -11,21 +11,21 @@ else
 end
 
 if not File.directory?('submodules/GenericContainer')
-  system('git clone --branch 1.1.2 --depth 1 https://github.com/SebastianoTaddei/GenericContainer.git submodules/GenericContainer')
+  system('git clone --branch 1.1.4 --depth 1 https://github.com/SebastianoTaddei/GenericContainer.git submodules/GenericContainer')
   system('ruby submodules/GenericContainer/setup.rb')
 else
   puts "GenericContainer already cloned. If you want to update it, delete the folder and run this script again."
 end
 
 if not File.directory?('submodules/UtilsLite')
-  system('git clone --branch 1.0.2 --depth 1 https://github.com/SebastianoTaddei/UtilsLite.git submodules/UtilsLite')
+  system('git clone --branch 1.0.4 --depth 1 https://github.com/SebastianoTaddei/UtilsLite.git submodules/UtilsLite')
   system('ruby submodules/UtilsLite/setup.rb')
 else
   puts "UtilsLite already cloned. If you want to update it, delete the folder and run this script again."
 end
 
 if not File.directory?('submodules/quarticRootsFlocke')
-  system('git clone --branch 1.1.2 --depth 1 https://github.com/SebastianoTaddei/quarticRootsFlocke.git submodules/quarticRootsFlocke')
+  system('git clone --branch 1.1.4 --depth 1 https://github.com/SebastianoTaddei/quarticRootsFlocke.git submodules/quarticRootsFlocke')
   system('ruby submodules/quarticRootsFlocke/setup.rb')
 else
   puts "quarticRootsFlocke already cloned. If you want to update it, delete the folder and run this script again."
